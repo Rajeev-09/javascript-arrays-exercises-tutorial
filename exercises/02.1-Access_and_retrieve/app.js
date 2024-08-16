3964b29a3e089bda657fd1404b9ae2edfd90abf1
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-08-16 06:53:34 +0000

    Access and retrieve data in array

--- exercises/02.1-Access_and_retrieve/app.js
+++ exercises/02.1-Access_and_retrieve/app.js
@@ -3,6 +3,16 @@ let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','satu
 
 // 1. print the 3rd item here
 
+// console.log(myArray[2]);
+
+
 // 2. change the 'thursday' value to null here
+let chn_Value = myArray[4] = null
+
+// console.log(chn_Value);
+
 
 // 3. print the position of step 2
+
+
+console.log(myArray[4]);
