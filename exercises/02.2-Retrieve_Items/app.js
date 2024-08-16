e8fc9a1a1caf3c39c7c3f1e8055709293f46c184
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-08-16 06:58:53 +0000

    Retrieve Item

--- exercises/02.2-Retrieve_Items/app.js
+++ exercises/02.2-Retrieve_Items/app.js
@@ -1,3 +1,16 @@
 let arr = [4,5,734,43,45,100,4,56,23,67,23,58,45,3,100,4,56,23];
 
 // your code here
+
+// 1. Print on the console the 1st element of the array.
+
+console.log(arr[0]);
+
+
+
+// 2. Print on the console the 4th element of the array.
+
+console.log(arr[3]);
+
+
+
