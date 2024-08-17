252085670fe9dc413c3e7ee822da4b65eeb27e1f
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-08-17 11:17:48 +0000

    Add item to array

--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -2,7 +2,19 @@ let arr = [4,5,734,43,45];
 
 // Your code here, use the push() function and the 2 Math functions
 
-arr.push(Math.floor(Math.random()*100))
-arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+// arr.push(Math.floor(Math.random()*100))
+
+for (let i = 0; i < 10; i++) {
+    arr.push(Math.floor(Math.random()*100))
+}
 
 console.log(arr);
