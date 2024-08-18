693c09682d559622322bb7338c022bddbfcdada9
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-08-18 06:45:47 +0000

    Loop and add items to array by looping

--- exercises/04.4-Add_items_to_array_looping/app.js
+++ exercises/04.4-Add_items_to_array_looping/app.js
@@ -3,7 +3,7 @@ let arr = [4,5,734,43,45];
 //*****************
 // Your code here
 
-for (let i = 0; i < 10; i++) {
+for (let i = 1; i <= 10; i++) {
         arr.push(Math.floor(Math.random()*100))
 }
 console.log(arr);
