8ebf2a7980c89a638ae2442fe6f84f4c4ca4c9c3
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-08-19 02:57:43 +0000

    Print the last one

--- exercises/03-Print_the_last_one/app.js
+++ exercises/03-Print_the_last_one/app.js
@@ -1,10 +1,17 @@
-function generateRandomArray()
-{
-	let auxArray = [];
-	let randomLength = Math.floor(Math.random()*100);
-	for(let i = 0; i < randomLength; i++) auxArray.push(Math.floor(Math.random()*100));
-	return auxArray;	
+function generateRandomArray() {
+    let auxArray = [];
+    let randomLength = Math.floor(Math.random() * 100);
+    // console.log(randomLength);
+    for (let i = 1; i < randomLength; i++){
+        hi = auxArray.push(Math.floor(Math.random() * 100)); 
+        // console.log(hi);
+    }
+    console.log(auxArray);	
+    return auxArray; 
 }
-let myStupidArray = generateRandomArray();
 
+let myStupidArray = generateRandomArray();
 // Your code here
+let theLastOne = myStupidArray[myStupidArray.length - 1];
+console.log(theLastOne)
+
