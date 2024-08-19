21a44cd09c337510ebd394753d64ed6a2f6ae75f
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-08-19 03:26:48 +0000

    Reversing of array

--- exercises/06.2-Loop-from-the-top/app.js
+++ exercises/06.2-Loop-from-the-top/app.js
@@ -1,7 +1,22 @@
 let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
+let lastPos = mySampleArray.length 
+let endPos = 0
 
-for(let i = 0; i < mySampleArray.length; i = i + 1)
-{
+for (let i = lastPos; i >= endPos; i = i -1) {
 	console.log(mySampleArray[i]);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
